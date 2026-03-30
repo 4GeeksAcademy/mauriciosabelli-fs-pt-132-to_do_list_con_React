43032a9865b770a74233d936d1e28aa947472d55
Mauricio Sabelli Machado <mnsabelli@gmail.com>
2026-03-30 21:13:05 +0000

    lista de tareas terminada

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,106 +1,81 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { faPen } from "@fortawesome/free-solid-svg-icons";
-
-
 
 const Home = () => {
+    const [input, setInput] = useState("");
+    const [todoList, setTodoList] = useState([]);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
 
-	const [input, setInput] = useState("");
-	const [todoList, setTodoList] = useState([]);
+    const handleDelete = (index) => {
+        const nuevaLista = todoList.filter((_, i) => i !== index);
+        setTodoList(nuevaLista);
+    };
 
-	const [editIndex, setEditIndex] = useState(null);
-	const [editValue, setEditValue] = useState("");
+    const handleChange = (event) => {
+        setInput(event.target.value);
+    };
 
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            if (input.trim() !== "") {
+                setTodoList([...todoList, input]);
+                setInput("");
+            } else {
+                alert("El campo no puede estar vacío");
+            }
+        }
+    };
 
-	const handleDelete = (index) => {
-		const nuevaLista = todoList.filter((_, i) => i !== index);
-		setTodoList(nuevaLista);
-	};
+    
 
+    return (
+        <div className="container-fluid row m-0 p-0">
+            <div className="col-sm-12 col-md-9 col-lg-6 align-content-center m-auto">
+                <p className="fs-3 mt-2">Agregar tarea</p>
+                <div className="d-flex">
+                    <input
+                        value={input} className="form-control" type="text"
+                        onChange={handleChange} onKeyDown={handleKeyDown}
+                        placeholder="Escribe la tarea y pulsa enter para añadirla a la lista"
+                    />
+                </div>
+                <ul className="list-group">
+                    <div>
+                        <p className="fs-3 mt-4">Lista de Tareas</p>
+                    </div>
 
-	const handleChange = (event) => {
-		setInput(event.target.value)
-	}
-
-
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		//Antes de agregarlo, vamos a verificar que el input no este vacio
-		if (input.trim() !== "") {
-			//Aca se agrega
-			setTodoList([...todoList, input])
-			setInput("");
-		} else {
-			//Mensaje de error
-			alert("Este campo no puede estar vacío")
-		}
-
-	}
-
-	const handleEdit = (index) => {
-		setEditIndex(index);
-		setEditValue(todoList[index]);
-	};
-
-	const handleSave = (index) => {
-		const nuevaLista = [...todoList];
-		nuevaLista[index] = editValue;
-		setTodoList(nuevaLista);
-		setEditIndex(null);
-		setEditValue("");
-	};
-
-
-	return (
-		<div className="container row">
-			<div className="col-sm-12 col-md-9 col-lg-6 align-content-center m-auto">
-				<p className="fs-3 mt-2">Nueva tarea</p>
-				<form className="d-flex" onSubmit={handleSubmit}>
-					<input value={input} className="form-control" type="text" onChange={handleChange}></input>
-					<button type="submit" className="btn btn-outline-primary ms-2">Añadir</button>
-				</form>
-				<ul className="list-group">
-					<div>
-						<p className="fs-3 mt-4">Tareas pendientes</p>
-					</div>
-
-					{todoList.map((todo, i) => (
-						<li className="list-group-item d-flex justify-content-between align-items-center m-1 pe-1" key={i}>
-							{editIndex === i ? (
-								<>
-									<input
-										className="form-control me-2"
-										value={editValue}
-										onChange={(e) => setEditValue(e.target.value)}
-									/>
-									<button onClick={() => handleSave(i)} className="btn btn-success">
-										Guardar
-									</button>
-								</>
-							) : (
-								<>
-									{todo}
-									<div className="d-flex gap-1">
-										<button onClick={() => handleEdit(i)} className="btn btn-primary">
-											<FontAwesomeIcon icon={faPen} />
-										</button>
-										<button onClick={() => handleDelete(i)} className="btn btn-primary">
-											<FontAwesomeIcon icon={faTrash} />
-										</button>
-									</div>
-								</>
-							)}
-						</li>
-					))}
-				</ul>
-				<p className="text-muted text-center mt-2 me-2">
-    				{todoList.length} tareas pendientes
-				</p>
-			</div>
-		</div>
-	);
+                    {todoList.length === 0 ? (
+                        <p className="text-muted text-center mt-2">
+                            No hay tareas pendientes
+                        </p>
+                    ) : (
+                        todoList.map((todo, i) => (
+                            <li
+                                className="list-group-item d-flex justify-content-between align-items-center shadow-sm"
+                                key={i}
+                                onMouseEnter={() => setHoveredIndex(i)}
+                                onMouseLeave={() => setHoveredIndex(null)}
+                            >
+                                {todo}
+                                <div className="d-flex gap-1" style={{ visibility: hoveredIndex === i ? "visible" : "hidden" }}>
+                                    <button onClick={() => handleDelete(i)} className="delete">
+                                        X
+                                    </button>
+                                </div>
+                            </li>
+                        ))
+                    )}
+                </ul>
+                
+                {todoList.length > 0 && (
+                    <p className="text-muted text-start m-3">
+                {todoList.length === 1 ? "1 tarea pendiente" : `${todoList.length} tareas pendientes`}
+                    </p>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
